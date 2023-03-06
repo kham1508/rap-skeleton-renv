2ffa40cfa477cd580b21a2af0d3539da065a99de
kham1508 <107246450+kham1508@users.noreply.github.com>
2023-03-06 12:17:47 +0000

    change to installs to work with renv

--- code/config.R
+++ code/config.R
@@ -35,26 +35,36 @@ header_frequency = "Ad-Hoc"
 
 # check list of packages and install if needed before loading
 # if package already installed will be loaded
-f_install_and_load_packages <- function(packages) {
-  installed_packages <- rownames(installed.packages())
-  packages_to_install <- packages[!packages %in% installed_packages]
-  
-  if (length(packages_to_install) > 0) {
-    message(paste0("Installing packages: ", paste(packages_to_install, collapse = ", ")))
-    install.packages(packages_to_install)
-  } else {
-    message("All packages already installed.")
-  }
-  
+# f_install_and_load_packages <- function(packages) {
+#   installed_packages <- rownames(installed.packages())
+#   packages_to_install <- packages[!packages %in% installed_packages]
+#   
+#   if (length(packages_to_install) > 0) {
+#     message(paste0("Installing packages: ", paste(packages_to_install, collapse = ", ")))
+#     install.packages(packages_to_install)
+#   } else {
+#     message("All packages already installed.")
+#   }
+#   
+#   # load all packages, whether newly installed or not
+#   for (pkg in packages) {
+#     library(pkg, character.only = TRUE, quietly = TRUE)
+#   }
+# }
+
+f_load_packages <- function(packages) {
   # load all packages, whether newly installed or not
   for (pkg in packages) {
     library(pkg, character.only = TRUE, quietly = TRUE)
   }
 }
 
-f_install_and_load_packages(c("dplyr","tidyr", "stringr","forcats","xfun","htmltools",
-                            "openxlsx","readxl","lubridate","plotly","here","kableExtra","scales","ggrepel",
-                            "DT","AMR","ggpubr","sf","tmap","htmltools","formattable","here"))
+# f_install_and_load_packages(c("dplyr","tidyr", "stringr","forcats","xfun","htmltools",
+#                             "openxlsx","readxl","lubridate","plotly","here","kableExtra","scales","ggrepel",
+#                             "DT","AMR","ggpubr","sf","tmap","htmltools","formattable","here"))
+f_load_packages(c("dplyr","tidyr", "stringr","forcats","xfun","htmltools",
+                              "openxlsx","readxl","lubridate","plotly","here","kableExtra","scales","ggrepel",
+                              "DT","AMR","ggpubr","sf","tmap","htmltools","formattable","here"))
 
 # turn off warning messages
 options(warn=-1)
